891f055d9de5639f95686981a9a55d04256ce035
Laura Antunez <lauraantunez024@gmail.com>
2022-07-30 14:35:44 -0400

    deploys without errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,40 +1,35 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("Update DB")
+  console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
-  const text = jateDb.transaction("jate", "readwrite");
-  const store = text.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
-  console.log("Database: ", result.value);
-}
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-  console.log("Get DB");
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const result = await store.put({ id: 1, value: content });
+  console.log("Data saved to the database", result);
+};
+
+export const getDb = async () => {
+  console.log("GET from the database");
   const jateDb = await openDB("jate", 1);
-  const text = jateDb.transaction("jate", "readonly");
-  const store = text.objectStore("jate");
-  const request = store.get(1);
-  const result = await request;
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const result = await store.get(1);
   result
-    ? console.log("Results:", result.value)
-    : console.log("No data found")
-  return result.value
-
-
-
+    ? console.log("Data retrieved from the database", result.value)
+    : console.log("Data not found in the database");
+  return result?.value;
+};
 initdb();
